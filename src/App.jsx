567c71c3ff53e940cc60517e59699cbfd21b7317
dladden <dladden77@gmail.com>
2023-10-21 17:08:30 -0400

    setting up layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
 import React from 'react';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import HomeLayout from './pages/HomeLayout';
+
+//Route function (provided by react) contains routs as objects in an array
+//which displays what is shown in the url of the page ("/" is a home page)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+  },
+  {
+    path: '/about',
+    element: (
+      <div>
+        <h1>about</h1>
+      </div>
+    ),
+  },
+]);
 
 const App = () => {
-  return <h1>Trend Flow</h1>;
+  //using RouterProvided and creating instance of router from above
+  return <RouterProvider router={router} />;
 };
 
 export default App;
